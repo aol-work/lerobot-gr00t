2865a66ae8379a44eff12545832189f74bc3a8f7
Alexander Oldemeier (work) <alexander.oldemeier@tum.de>
2025-06-10 08:55:07 +0200

    Add main function

--- eval_gr00t_on_so101.py
+++ eval_gr00t_on_so101.py
@@ -20,6 +20,15 @@ from gr00t.eval.service import ExternalRobotInferenceClient
 MODALITY_KEYS = ["single_arm", "gripper"]
 ACTION_HORIZON = 12
 ACTIONS_TO_EXECUTE = 100
+MOTOR_POSITION_NAMES = [
+    "shoulder_pan.pos",
+    "shoulder_lift.pos",
+    "elbow_flex.pos",
+    "wrist_flex.pos",
+    "wrist_roll.pos",
+    "gripper.pos",
+]
+
 
 class Gr00tRobotInferenceClient:
     """
@@ -70,84 +79,84 @@ class Gr00tRobotInferenceClient:
         self.language_instruction = lang_instruction
 
 
-MOTOR_POSITION_NAMES = [
-    "shoulder_pan.pos",
-    "shoulder_lift.pos",
-    "elbow_flex.pos",
-    "wrist_flex.pos",
-    "wrist_roll.pos",
-    "gripper.pos",
-]
-
-
 def numpy_to_dict(tensor: np.ndarray) -> dict:
     """
-    Convert a tensor to a dictionary.
+    Convert a tensor containing motor positions to a dictionary.
     """
     return {MOTOR_POSITION_NAMES[i]: tensor[i] for i in range(len(MOTOR_POSITION_NAMES))}
 
 
 def dict_to_numpy(d: dict) -> np.ndarray:
     """
-    Convert a dictionary to a tensor.
+    Convert a dictionary containing motor positions to a tensor.
     """
     return np.array([d[key] for key in MOTOR_POSITION_NAMES])
 
 
-def move_to_initial_pose(f: SO101Follower):
+def go_home(follower: SO101Follower):
     """
     Move the robot to the initial pose.
     """
     current_motor_state = numpy_to_dict(np.array([0, -90, 90, 90, 0, 0]))
-    f.send_action(current_motor_state)
+    follower.send_action(current_motor_state)
     time.sleep(1)
     print("-------------------------------- moving to initial pose")
 
 
-config = SO101FollowerConfig(
-    port="/dev/motor-bus-follower",
-    id="follower12vblack",
-    cameras={
-        "webcam": OpenCVCameraConfig(
-            index_or_path=2,
-            fps=30,
-            width=640,
-            height=480,
-            color_mode=ColorMode.RGB,
-            rotation=Cv2Rotation.NO_ROTATION,
-        )
-    },
-)
-
-# TODO(aol-work): Get optional config from the command line
-client = Gr00tRobotInferenceClient()
-follower = SO101Follower(config)
-follower.connect(calibrate=False)
-
-move_to_initial_pose(follower)
-
-try:
-    for i in tqdm(range(ACTIONS_TO_EXECUTE), desc="Executing actions"):
-        observation = follower.get_observation()
-        img = observation["webcam"]
-        state = dict_to_numpy(observation)
-        action = client.get_action(img, state)
-        start_time = time.time()
-        for i in range(ACTION_HORIZON):
-            concat_action = np.concatenate(
-                [np.atleast_1d(action[f"action.{key}"][i]) for key in MODALITY_KEYS],
-                axis=0,
+def main():
+    """
+    Main function to run the Gr00t robot on the SO101 follower.
+    """
+    # TODO(aol-work): Get optional config from the command line
+    config = SO101FollowerConfig(
+        port="/dev/motor-bus-follower",
+        id="follower12vblack",
+        cameras={
+            "webcam": OpenCVCameraConfig(
+                index_or_path=2,
+                fps=30,
+                width=640,
+                height=480,
+                color_mode=ColorMode.RGB,
+                rotation=Cv2Rotation.NO_ROTATION,
             )
-            assert concat_action.shape == (6,), concat_action.shape
-            follower.send_action(numpy_to_dict(concat_action))
-            time.sleep(0.02)
+        },
+    )
 
-            # 0.05*16 = 0.8 seconds
-            print("executing action", i, "time taken", time.time() - start_time)
-        print("Action chunk execution time taken", time.time() - start_time)
-except KeyboardInterrupt:
-    print("\nGracefully exiting on CTRL-C")
+    client = Gr00tRobotInferenceClient()
+    follower = SO101Follower(config)
+    follower.connect(calibrate=False)
 
-move_to_initial_pose(follower)
+    go_home(follower)
 
-follower.disconnect()
+    try:
+        for i in tqdm(range(ACTIONS_TO_EXECUTE), desc="Executing actions"):
+            observation = follower.get_observation()
+            img = observation["webcam"]
+            state = dict_to_numpy(observation)
+            action = client.get_action(img, state)
+            start_time = time.time()
+            for i in range(ACTION_HORIZON):
+                concat_action = np.concatenate(
+                    [np.atleast_1d(action[f"action.{key}"][i])
+                     for key in MODALITY_KEYS],
+                    axis=0,
+                )
+                assert concat_action.shape == (6,), concat_action.shape
+                follower.send_action(numpy_to_dict(concat_action))
+                time.sleep(0.02)
+
+                # 0.05*16 = 0.8 seconds
+                print("executing action", i, "time taken",
+                      time.time() - start_time)
+            print("Action chunk execution time taken", time.time() - start_time)
+    except KeyboardInterrupt:
+        print("\nGracefully exiting on CTRL-C")
+
+    go_home(follower)
+
+    follower.disconnect()
+
+
+if __name__ == "__main__":
+    main()
