54d3ef9570040d0dd54a6ebd824098a9c85d403f
Alexander Oldemeier (work) <alexander.oldemeier@tum.de>
2025-06-13 08:43:35 +0200

    Add back some configuration options

--- eval_gr00t_on_so101.py
+++ eval_gr00t_on_so101.py
@@ -2,6 +2,7 @@
 This script is an example to run the Gr00t robot on the SO101 follower.
 """
 
+import argparse
 import time
 
 import numpy as np
@@ -22,8 +23,6 @@ from gr00t.eval.service import ExternalRobotInferenceClient
 
 
 MODALITY_KEYS = ["single_arm", "gripper"]
-ACTION_HORIZON = 12
-ACTIONS_TO_EXECUTE = 100
 MOTOR_POSITION_NAMES = [
     "shoulder_pan.pos",
     "shoulder_lift.pos",
@@ -111,13 +110,31 @@ def main():
     """
     Main function to run the Gr00t robot on the SO101 follower.
     """
-    # TODO(aol-work): Get optional config from the command line
+    parser = argparse.ArgumentParser(
+        description="Run the Gr00t robot on the SO101 follower")
+    parser.add_argument("--host", type=str, default="localhost",
+                        help="Host for the Gr00t robot service")
+    parser.add_argument("--port", type=int, default=5555,
+                        help="Port for the Gr00t robot service")
+    parser.add_argument("--action_horizon", type=int, default=12,
+                        help="Number of actions to execute per step")
+    parser.add_argument("--actions_to_execute", type=int,
+                        default=100, help="Total number of actions to execute")
+    parser.add_argument("--camera_idx", type=int,
+                        default=2, help="Camera index to use")
+    parser.add_argument("--lang_instruction", type=str, default="Move the green cylinder towards the grey spot",
+                        help="Language instruction for the robot")
+    args = parser.parse_args()
+
+    # Print language instruction for confirmation
+    print("Language instruction:", args.lang_instruction)
+
     config = SO101FollowerConfig(
         port="/dev/motor-bus-follower",
         id="follower12vblack",
         cameras={
             "webcam": OpenCVCameraConfig(
-                index_or_path=2,
+                index_or_path=args.camera_idx,
                 fps=30,
                 width=640,
                 height=480,
@@ -127,7 +144,11 @@ def main():
         },
     )
 
-    client = Gr00tRobotInferenceClient()
+    client = Gr00tRobotInferenceClient(
+        host=args.host,
+        port=args.port,
+        language_instruction=args.lang_instruction
+    )
     follower = SO101Follower(config)
     # TODO(aoldemeier): Add optional calibration
     follower.connect(calibrate=False)
@@ -137,13 +158,13 @@ def main():
     go_home(follower)
 
     try:
-        for i in tqdm(range(ACTIONS_TO_EXECUTE), desc="Executing actions"):
+        for i in tqdm(range(args.actions_to_execute), desc="Executing actions"):
             observation = follower.get_observation()
             img = observation["webcam"]
             state = dict_to_numpy(observation)
             action = client.get_action(img, state)
             start_time = time.time()
-            for i in range(ACTION_HORIZON):
+            for i in range(args.action_horizon):
                 concat_action = np.concatenate(
                     [np.atleast_1d(action[f"action.{key}"][i])
                      for key in MODALITY_KEYS],
